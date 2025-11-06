674cdcb939dce56759d32a49ce4a8ae5e1e0610f
sneh-303 <snehshial@gmail.com>
2025-11-06 22:26:27 +0530

    Fix Netlify 404 issue and update frontend

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "https://task-tracker-app-trbn.onrender.com/api",
 });
 
 export default api;
